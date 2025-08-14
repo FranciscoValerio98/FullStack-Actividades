ceebad1b186bcf623ff4238b1128202302d435e2
FranciscoValerio98 <U22210871@utp.edu.pe>
2025-08-14 17:28:47 -0500

    Primer commit - Subiendo archivos

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("year").textContent = new Date().getFullYear();
+
+function agregar(nombre) {
+  alert(`✅ Agregado al carrito: ${nombre}`);
+}
+
+function info(nombre) {
+  alert(`ℹ️ Más información sobre: ${nombre}`);
+}
